75b7137acaa8b586feb42c2e9dec7d3716a4c57c
roderick <rod@nete.com>
2018-04-04 11:07:25 -0400

    First push

---
+++ script.js
@@ -0,0 +1,44 @@
+//Variables coding
+
+/*var name='John';
+console.log(name);
+
+var lastName = 'Smith';
+console.log(lastName);
+
+var age = 26;
+console.log(age);
+
+var fullAge = false;
+console.log(fullAge);
+*/
+
+//Lecture variables 2
+/*
+var name='John';
+var age=26;
+
+//console.log(name + " " + age);
+//console.log(age + " " + age);
+//console.log(age + age);
+
+var job, isMarried;
+
+//console.log(job);
+
+job = 'teacher';
+isMarried = false;
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+age = 'thirsty six';
+job = 'driver';
+
+console.log(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+
+//var lastName = prompt('What is the last name?');
+//console.log(lastName);
+
+alert(name + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried + '.');
+*/
+

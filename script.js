230700398667f4976be6b27a656825222885f88e
roderick <rod@nete.com>
2018-04-10 10:13:55 -0400

    going again

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ if (23 === "23") {
 
 //////////Lecture on boolena logic and switch
 
-var age = 35;
+/*var age = 35;
 
 if (age < 20) {
     console.log('John is a teenager');
@@ -124,3 +124,78 @@ switch (job) {
         console.log('John does something else.')
         
 }
+*/
+/*
+var heightJohn = 172;
+var heightMike = 190;
+var ageJohn = 76;
+var ageMike = 29;
+
+var scoreJohn = heightJohn + 5 * ageJohn;
+var scoreMike = heightMike + 5 * ageMike;
+*/
+/*if (scoreJohn > scoreMike) {
+    console.log('John wins the game with ' + scoreJohn + ' points!');
+} else if (scoreMike > scoreJohn) {
+    console.log('Mike wins the game with ' + scoreMike + ' points!');
+} else if (scoreJohn === scoreMike) {
+    console.log('There is a draw.');
+}
+*/
+/*
+var heightMary = 158;
+var ageMary = 90;
+var scoreMary = heightMary + 5 * ageMary;
+
+if (scoreJohn > scoreMike && scoreJohn > scoreMary) {
+    console.log('John wins the game with ' + scoreJohn + ' points!');
+} else if (scoreMike > scoreJohn && scoreMike > scoreMary) {
+    console.log('Mike wins the game with ' + scoreMike + ' points!');
+} else if (scoreMary > scoreJohn && scoreMary > scoreMike) {
+    console.log('Mary wins the game with ' + scoreMike + ' points!');
+} else  {
+    console.log('It\'s a draw.');
+}
+*/
+
+////Functions
+
+/*function calculateAge(yearOfBirth) {
+    var age = 2016 - yearOfBirth;
+    return age;
+}
+
+var ageJohn = calculateAge(1990);
+var ageMike = calculateAge(1969);
+var ageMary = calculateAge(1949);
+
+
+function yearsUntilRetirement(name,year) {
+   var age = calculateAge(year);
+   var retirement = 65 - age;
+    if (retirement <= 0) {
+    console.log(name + ' has already retired.');
+} else  
+    console.log(name + ' retires in ' + retirement + ' years.');
+}
+
+yearsUntilRetirement('John', 1990);
+yearsUntilRetirement('Mike', 1969);
+yearsUntilRetirement('Mary', 1949);
+*/
+
+for (var i = 0; i < 80; i++) {     
+    console.log("The number is " + i + "<br>"); 
+}
+
+
+/*
+var distanceFromFtoS = 800;
+var totalDistance = distanceFromFtoS;
+
+ if (totalDistance <= 39) {
+    console.log('The total distance is only ' + totalDistance + ' so my black ass is visiting.');
+} else  { 
+    console.log(totalDistance + ' is too long to drive!!! FUCK THAT!!!');
+}
+*/
